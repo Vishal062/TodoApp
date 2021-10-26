e8693e5cad649f13adf93c936a2ce02e6dc2b391
Vishal062 <vs9425348@gmail.com>
2021-10-27 00:57:54 +0530

    React hook ToDo App

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Counter from "./Counter/Counter";
+import { Example } from "./Counter/Example";
+import { Todo } from "./Counter/Todo";
+import "./styles.css";
+
+export default function App() {
+  const [showCounter, setShowCounter] = React.useState(false);
+  return (
+    <div className="App">
+      {/* { {showCounter <Counter />}
+      <br />
+      <button onClick={() => setShowCounter(!showCounter)}>SHOW</button>
+      <Example /> } */
+      }
+      
+
+      <Todo />
+    </div>
+  );
+}
+
+// React.useEffect
